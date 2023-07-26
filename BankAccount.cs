1d71dd9457fa49f48479bd5115a1c66e89b4bc09
unknown <kbhoomika05@gmail.com>
2023-07-26 15:43:59 +0530

    Assignment 16 Properties of C#

---
+++ BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16_PropertiesOfC_
+{
+    public class BankAccount
+    {
+		private int accountNumber;
+
+		public int AccountNumber
+		{
+			get { return accountNumber; }
+		}
+		private string accountHolderName;
+
+		public string AccountHolderName
+		{
+			get { return accountHolderName; }
+			set { accountHolderName = value; }
+		}
+		private double balance;
+
+		public double Balance
+		{
+			get { return balance; }
+		}
+        public BankAccount(string accountHolderName)
+        {
+			this.accountHolderName = accountHolderName;
+            balance = 0;
+        }
+		public void Deposit()
+		{
+            Console.WriteLine("Enter Amount to Deposit: ");
+			double amount = double.Parse(Console.ReadLine());
+			balance += amount;
+            Console.WriteLine("Account Holder Name: "+accountHolderName);
+            Console.WriteLine("Balance Amount= "+balance);
+        }
+		public void Withdraw()
+		{
+            Console.WriteLine("Enter Amount to Withdraw: ");
+			double amount = double.Parse(Console.ReadLine());
+			balance -= amount;
+            Console.WriteLine("Account Holder Name: " + accountHolderName);
+            Console.WriteLine("Balance Amount= " + balance);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment16_PropertiesOfC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount bankAccount = new BankAccount("Bhoomika");
+            //Console.WriteLine("Enter Name: ");
+            //bankAccount.AccountHolderName = Console.ReadLine();
+            bankAccount.Deposit();
+            bankAccount.Withdraw();
+            Console.ReadKey();
+
+        }
+    }
+}
